0ad41f38f5a19c36c269c99a6bfaa0c553253581
GxAditya <kradi2645@gmail.com>
2025-11-21 05:24:35 +0530

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/Devlog/' : '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
   },
 });
